9cc9c6ae29b12a8736e527919e2f103154b1511e
Jonathan <jonathan.gustafsson@hotmail.com>
2022-10-21 18:23:24 +0200

    blocknr

--- src/hooks/useEnsRegistrations.ts
+++ src/hooks/useEnsRegistrations.ts
@@ -1,14 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
+import { useBlockNumber } from 'wagmi';
 import { getRegistrations } from '../utils/api';
 
 const useEnsRegistrations = () => {
   const [lastBlock, setLastBlock] = useState<number>();
+  useBlockNumber({
+    onBlock: () => {
+      setLastBlock(lastBlock);
+    },
+    watch: false,
+  });
+
   const query = useQuery(
     ['registrations', lastBlock],
     () => getRegistrations(lastBlock),
     {
       refetchInterval: 7000,
+      enabled: Boolean(lastBlock),
       onSuccess: (res) => {
         console.log(res);
         if (res[0]) {
